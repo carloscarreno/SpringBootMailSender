87fb290d419826101a569ad8772876589744888e
Carlos Augusto Carreño Villarreyes <ccarreno@INDECI.GOB.PE>
2024-07-22 09:08:47 -0500

    actualizar

--- src/main/java/io/redeasy/mail/sender/SpringBootMailSenderApplication.java
+++ src/main/java/io/redeasy/mail/sender/SpringBootMailSenderApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SprinBootMailSenderApplication {
+public class SpringBootMailSenderApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SprinBootMailSenderApplication.class, args);
+		SpringApplication.run(SpringBootMailSenderApplication.class, args);
 	}
 
 }
